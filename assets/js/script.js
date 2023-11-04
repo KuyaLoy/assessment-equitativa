65bf022f0c129f923fc37f5f5f32116b1b124ebb
KuyaLoy <robintapiru0894@gmail.com>
2023-11-05 03:27:44 +0400

    First Step - Done
    First Step - Done

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,56 @@ var mySwiper = new Swiper(".swiper-container", {
   noSwiping: true, // Disable swiping by default
   noSwipingClass: "no-swipe", // Apply swiping to elements with this class
 });
+
+$(document).ready(function () {
+  $('input[type="radio"][name="position-available"]').click(function () {
+    // Check if any radio button in the group has been selected
+    if (
+      $('input[type="radio"][name="position-available"]:checked').length > 0
+    ) {
+      // Remove the "disabled" attribute from a button with a specific ID
+      $(".next").removeAttr("disabled");
+      $("#progressbar li:nth-child(1)").addClass("active");
+    }
+  });
+
+  let nextCount = 1;
+
+  $(".next").click(function () {
+    if (nextCount < 5) {
+      nextCount++;
+
+      $(".steps").removeClass("active");
+      $("#step-" + nextCount + "").addClass("active");
+
+      if (nextCount == 2) {
+        $(".prev").addClass("active");
+      }
+      if (nextCount == 5) {
+        $(".next").addClass("hide");
+        $(".send-application").addClass("active");
+      }
+      $("#progressbar li:nth-child(" + nextCount + ")").addClass("active");
+    }
+    console.log(nextCount);
+  });
+  $(".prev").click(function () {
+    $("#progressbar li:nth-child(" + nextCount + ")").removeClass("active");
+    if (nextCount > 1) {
+      nextCount--;
+      $(".steps").removeClass("active");
+      $("#step-" + nextCount + "").addClass("active");
+
+      if (nextCount == 1) {
+        $(this).removeClass("active");
+        // $(".next").attr("disabled", true);
+      }
+      if (nextCount == 4) {
+        $(".next").removeClass("hide");
+        $(".send-application").removeClass("active");
+      }
+    }
+
+    console.log(nextCount);
+  });
+});
